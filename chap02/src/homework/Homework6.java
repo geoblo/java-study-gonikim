6a316827a1ca3b12bfaf17f0dfe94ff33577e096
geoblo <geoblo.naver.com>
2025-07-17 09:50:52 +0900

    풀이

---
+++ chap02/src/homework/Homework6.java
@@ -0,0 +1,20 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Homework6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("한 자리 숫자를 입력하세요: ");
+		char digitChar = sc.nextLine().charAt(0);
+		
+		// char 타입의 숫자형 문자를 정수로 변환하여 계산
+		int digit = digitChar - '0'; // 연산 시 자동 형변환(char -> int)이 발생
+		int square = digit * digit;
+		
+		System.out.println("입력한 숫자의 제곱은 " + square + "입니다.");
+	}
+
+}
