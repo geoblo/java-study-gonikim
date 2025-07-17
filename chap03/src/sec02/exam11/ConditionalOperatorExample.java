e1b3de73a9bca39e241b1a7081931f75947b54e2
geoblo <geoblo.naver.com>
2025-07-17 15:48:08 +0900

    quiz

---
+++ chap03/src/sec02/exam11/ConditionalOperatorExample.java
@@ -0,0 +1,26 @@
+package sec02.exam11;
+
+public class ConditionalOperatorExample {
+
+	public static void main(String[] args) {
+		int score = 75;
+		
+		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
+		System.out.println(score + "점은 " + grade + "등급입니다.");
+		
+		// Quiz
+//		0~100사이의 점수를 입력받아
+//		점수가 60점 이상이면 "합격",
+//		60점 미만인 경우 "불합격"을 출력하는 프로그램을 구현하시오.
+//		단 음수이거나 100이 넘는 점수는 "점수입력오류"로 출력한다.
+//
+//		[입력] [출력]
+//		 85 => 합격
+//		 50 => 불합격
+//		-10 => 점수입력오류
+		
+		
+		
+	}
+
+}
