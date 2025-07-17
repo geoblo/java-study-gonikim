58cb6fc0b01afc2f932d787f743da0a554d7524b
geoblo <geoblo.naver.com>
2025-07-17 16:37:09 +0900

    과제

--- chap03/src/sec02/exam11/ConditionalOperatorExample.java
+++ chap03/src/sec02/exam11/ConditionalOperatorExample.java
@@ -1,15 +1,18 @@
 package sec02.exam11;
 
+import java.util.Scanner;
+
 public class ConditionalOperatorExample {
 
 	public static void main(String[] args) {
 		int score = 75;
 		
+		// 값이 오는 자리에 연산식이 올 수도 있음
 		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
 		System.out.println(score + "점은 " + grade + "등급입니다.");
 		
 		// Quiz
-//		0~100사이의 점수를 입력받아
+//		0~100사이의 점수를 입력받아 삼항(조건) 연산자를 사용하여
 //		점수가 60점 이상이면 "합격",
 //		60점 미만인 경우 "불합격"을 출력하는 프로그램을 구현하시오.
 //		단 음수이거나 100이 넘는 점수는 "점수입력오류"로 출력한다.
@@ -19,8 +22,18 @@ public class ConditionalOperatorExample {
 //		 50 => 불합격
 //		-10 => 점수입력오류
 		
-		
-		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("점수를 입력하세요: ");
+        score = scanner.nextInt();
+        
+        String result = (score < 0 || score > 100) 
+        					? "점수입력오류" 
+						: (score >= 60) 
+							? "합격" 
+							: "불합격";
+        
+        System.out.println(result);
 	}
 
 }
