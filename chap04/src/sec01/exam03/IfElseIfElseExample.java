31a1fd538f0b77e83732dc1d58b31a7bf5fdf1fd
geoblo <geoblo.naver.com>
2025-07-18 10:25:11 +0900

    풀이

--- chap04/src/sec01/exam03/IfElseIfElseExample.java
+++ chap04/src/sec01/exam03/IfElseIfElseExample.java
@@ -1,5 +1,7 @@
 package sec01.exam03;
 
+import java.util.Scanner;
+
 public class IfElseIfElseExample {
 
 	public static void main(String[] args) {
@@ -35,8 +37,20 @@ public class IfElseIfElseExample {
 //		  0  => 숫자
 //		  가  => 기타문자
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("문자 하나를 입력하세요: ");
+		char ch = sc.nextLine().charAt(0);
 		
+		if (ch >= 'A' && ch <= 'Z') {
+			System.out.println("대문자");
+		} else if (ch >= 'a' && ch <= 'z') {
+			System.out.println("소문자");			
+		} else if (ch >= '0' && ch <= '9') {
+			System.out.println("숫자");			
+		} else {
+			System.out.println("기타문자");
+		}
 	}
 
 }
