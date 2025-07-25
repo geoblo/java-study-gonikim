85b4d135e32097229021953b7793726dced78762
geoblo <geoblo.naver.com>
2025-07-25 10:16:06 +0900

    up

--- chap05/src/homework/Homework4.java
+++ chap05/src/homework/Homework4.java
@@ -6,8 +6,35 @@ public class Homework4 {
 
 	public static void main(String[] args) {
 		// 공식화: (int) (Math.random() * (최대값 - 최소값 + 1)) + 최소값;
-		 
 		
+		int[] lottoArr = new int[6];
+		int count = 0;
+		
+		while (count < 6) {
+			int num = (int) (Math.random() * 45) + 1;
+			
+			// 중복 체크
+			boolean dup = false;
+			for (int i = 0; i < count; i++) { // 실제 로또 번호가 들어간 count까지만 체크
+				if (lottoArr[i] == num) {
+					dup = true;
+					break; // 중복되는 값을 찾은 즉시 빠져나감
+				}
+			}
+			
+			// 중복이 아니면 배열에 저장
+			if (!dup) {
+				lottoArr[count] = num;
+				count++;
+			}
+		}
+		
+		// 오름차순 정렬
+		Arrays.sort(lottoArr);
+		
+		for (int num : lottoArr) {
+			System.out.print(num + " ");
+		}
 	}
 
 }
---
+++ chap05/src/homework/Homework6.java
@@ -0,0 +1,71 @@
+package homework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homework6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("배열의 크기를 입력하세요: ");
+        int size = sc.nextInt();
+        sc.nextLine(); // 개행 문자 제거
+
+        String[] arr = new String[size];
+        
+        for (int i = 0; i < arr.length; i++) { // arr.length == size
+            System.out.print((i + 1) + "번째 문자열: ");
+            arr[i] = sc.nextLine();
+        }
+
+        while (true) {
+            System.out.print("더 값을 입력하시겠습니까?(Y/N): ");
+            String answer = sc.nextLine();
+
+            if (answer.equalsIgnoreCase("N")) {
+                break;
+            } else if (answer.equalsIgnoreCase("Y")) {
+                System.out.print("더 입력하고 싶은 개수: ");
+                int addCount = sc.nextInt();
+                sc.nextLine(); // 개행 문자 제거
+                
+                // Arrays.copyOf를 이용해 크기 확장
+                arr = Arrays.copyOf(arr, arr.length + addCount);
+
+                // 추가된 부분에 새 문자열 입력
+                for (int i = arr.length - addCount; i < arr.length; i++) {
+                    System.out.print((i + 1) + "번째 문자열: ");
+                    arr[i] = sc.nextLine();
+                }
+            } else {
+                System.out.println("입력할 수 없는 값입니다.");
+            }
+        }
+
+        // 전체 출력
+        for (String str : arr) {
+            System.out.println(str);
+        }
+	}
+
+}
+
+/* 직접 복사하는 방법
+// 새 배열 생성
+String[] newArr = new String[arr.length + addCount];
+
+// 기존 배열 복사
+for (int i = 0; i < arr.length; i++) {
+    newArr[i] = arr[i];
+}
+
+// 새 입력값 저장
+for (int i = arr.length; i < newArr.length; i++) {
+    System.out.print((i + 1) + "번째 문자열: ");
+    newArr[i] = sc.nextLine();
+}
+
+// 배열 교체
+arr = newArr;
+*/
