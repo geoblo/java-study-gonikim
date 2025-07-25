6721658e065e96992e285e469aabbf1414a49b41
geoblo <geoblo.naver.com>
2025-07-25 09:52:13 +0900

    up

--- chap05/src/homework/Homework2.java
+++ chap05/src/homework/Homework2.java
@@ -10,7 +10,18 @@ public class Homework2 {
 		System.out.print("배열의 길이 입력: ");
 		int length = sc.nextInt();
 		
+		int[] arr = new int[length];
 		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("배열 " + i + "번째 인덱스에 넣을 값: ");
+			arr[i] = sc.nextInt();
+		}
+		
+		int sum = 0;
+		for (int num : arr) {
+			System.out.print(num + " ");
+			sum += num;
+		}
 		
 		System.out.println("\n총합: " + sum);
 	}
