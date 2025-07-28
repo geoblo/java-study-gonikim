18128e078e8ceb72d576f94033d7247393093ae5
geoblo <geoblo.naver.com>
2025-07-28 10:09:49 +0900

    up

--- chap07/src/sec02/exam03/Car.java
+++ chap07/src/sec02/exam03/Car.java
@@ -18,4 +18,28 @@ public class Car {
 	
 	
 	// 메소드
+	// 4개의 타이어를 1회전 시키는 메소드
+	// 어떤 타이어가 펑크 났는지 정수값으로 리턴
+	int run() {
+		System.out.println("[자동차가 달립니다.]");
+		
+		// 어떤 객체를 참조하냐에 따라 roll()의 실행 결과는 달라짐 => 다형성
+		if (frontLeftTire.roll() == false) { stop(); return 1; }
+		if (frontRightTire.roll() == false) { stop(); return 2; }
+		if (backLeftTire.roll() == false) { stop(); return 3; }
+		if (backRightTire.roll() == false) { stop(); return 4; }
+		return 0;
+	}
+	
+	void stop() {
+		System.out.println("[자동차가 멈춥니다.]");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
