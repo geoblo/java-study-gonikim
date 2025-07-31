a0da235423c1e2d8dc8e2bc5a84aaa7a7c4cd761
geoblo <geoblo.naver.com>
2025-07-31 12:04:21 +0900

    quiz

---
+++ chap11/src/sec01/exam04/quiz/Student.java
@@ -0,0 +1,17 @@
+package sec01.exam04.quiz;
+
+public class Student {
+	private int studentNo;
+	private String name;
+	private String major;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "[studentNo=" + studentNo + ", name=" + name + ", major=" + major + "]";
+	}
+	
+	
+	
+}
