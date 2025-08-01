169410338ea6044290389df6695a2301995e1b41
geoblo <geoblo.naver.com>
2025-08-01 11:05:26 +0900

    up

---
+++ chap11/src/sec01/quiz/Quiz2.java
@@ -0,0 +1,24 @@
+package sec01.quiz;
+
+public class Quiz2 {
+//	Quiz
+//	실행 클래스의 deleteChar 메소드를 구현하시오
+//	deleteChar(원본문자열, 삭제문자열) 메소드는 원본 문자열에서 삭제 문자열에 속한 문자를 제거하고 
+//	남는 문자열을 리턴하는 메소드입니다.
+//	※ 참고: 정적(static) 메소드 안에서 인스턴스 메소드를 쓸 수 없습니다.
+//	
+//	[입력]
+//	1) 1,2-3 ,-!
+//	2) 1a2b3 12345
+//
+//	[출력]
+//	1) 123
+//	2) ab
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
