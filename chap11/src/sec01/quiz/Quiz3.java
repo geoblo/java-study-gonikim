929f255dc6d4d695776a5d1a4bb1beb2694f34fc
geoblo <geoblo.naver.com>
2025-08-01 12:52:57 +0900

    up

--- chap11/src/sec01/quiz/Quiz3.java
+++ chap11/src/sec01/quiz/Quiz3.java
@@ -1,5 +1,7 @@
 package sec01.quiz;
 
+import java.util.Scanner;
+
 public class Quiz3 {
 //	Quiz
 //	원본 문자열과 찾는 문자열을 입력 받아 개수를 출력하는 프로그램을 구현하기 위해 
@@ -10,19 +12,61 @@ public class Quiz3 {
 //	[입력]
 //	1) 123AB123 AB
 //	2) 123AB123 13
+//	3) 123AB123AB12 AB
 //
 //	[출력]
 //	1) 1
 //	2) 0
-
+//	3) 2
+//
 //  [힌트]
 //	indexOf(String str, int fromIndex)를 이용해 반복해서 찾기
 //	fromIndex: 검색 시작 위치 지정
 	
 	public static void main(String[] args) {
-		
-		
-		
+		Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        System.out.println(count(str1, str2));
 	}
+
+	private static int count(String original, String target) {
+		int count = 0;
+		int index = 0;
+		
+		// indexOf를 이용해 반복해서 찾기
+		index = original.indexOf(target);
+		while (index != -1) {
+			count++;
+			index += target.length(); // 다음 검색 위치로 이동
+			index = original.indexOf(target, index); // 다음 위치에서 다시 검색
+		}
+		
+		// 축약형
+		while ((index = original.indexOf(target, index)) != -1) {
+			count++;
+			index += target.length(); // 다음 검색 위치로 이동
+		}
+		
+		// 무한 반복문 사용 시
+		while (true) {
+			index = original.indexOf(target, index);
+			if (index == -1) break;
+			count++;
+			index += target.length(); // 다음 검색 위치로 이동
+		}
+		
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
