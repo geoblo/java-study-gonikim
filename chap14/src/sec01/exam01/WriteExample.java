22fdd6a16bca56609f521dd465066e93000c475c
geoblo <geoblo.naver.com>
2025-08-07 10:18:03 +0900

    up

---
+++ chap14/src/sec01/exam01/WriteExample.java
@@ -0,0 +1,20 @@
+package sec01.exam01;
+
+// 입출력 스트림이란?
+// Stream(스트림): 개울(시내), 흐르다 = 마치 물과 같이 데이터가 흘러서 지나감
+// 데이터를 연속된 흐름으로 읽고/쓰는 것으로 입출력 할 수 있게 해주는 기능(입출력을 도와주는 클래스)
+// Java에서는 스트림을 통해 파일, 네트워크, 키보드, 메모리 등에서 데이터를 주고받을 수 있음
+// 입력 스트림: 외부 -> 프로그램으로 데이터 읽기
+// 출력 스트림: 프로그램 -> 외부로 데이터 쓰기
+   
+// 스트림의 종류
+// 구분  바이트 기반(1byte)   문자 기반(2byte = char)
+// 입력  InputStream        Reader
+// 출력  OutputStream       Writer
+public class WriteExample {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
