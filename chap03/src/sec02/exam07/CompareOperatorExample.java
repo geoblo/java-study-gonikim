e0c6de0c66dd08cc73308b600118d49036243d04
geoblo <geoblo@naver.com>
2025-11-12 14:52:04 +0900

    up

--- chap03/src/sec02/exam07/CompareOperatorExample.java
+++ chap03/src/sec02/exam07/CompareOperatorExample.java
@@ -1,5 +1,7 @@
 package sec02.exam07;
 
+import java.util.Scanner;
+
 public class CompareOperatorExample {
 
 	public static void main(String[] args) {
@@ -27,20 +29,14 @@ public class CompareOperatorExample {
 //		 1 2 => false
 //		10 5 => true
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("두 개의 정수를 입력하세요: ");
+		int num3 = sc.nextInt();
+		int num4 = sc.nextInt();
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		boolean result = num3 >= num4;
+		System.out.println(result);
 	}
 
 }
