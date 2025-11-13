c428be3c098a75e1b0950566212ab5f7595239f1
geoblo <geoblo@naver.com>
2025-11-13 10:10:05 +0900

    up

---
+++ chap03/src/sec02/exam10/AssignmentOperatorExample.java
@@ -0,0 +1,40 @@
+package sec02.exam10;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		int result = 10;
+		
+		result += 10; // result = result + 10;
+		System.out.println("result=" + result);
+		
+		result -= 5; // result = result - 5;
+		System.out.println("result=" + result);
+		
+		result *= 3; // result = result * 3;
+		System.out.println("result=" + result);
+		
+		result /= 5; // result = result / 5;
+		System.out.println("result=" + result);
+		
+		result %= 3; // result = result % 3;
+		System.out.println("result=" + result);
+		
+		// Quiz
+//		금액을 입력받아 500원, 100원, 50원, 10원, 1원 동전으로 변경하려고 합니다.
+//		최소 필요한 동전의 개수를 출력하는 프로그램을 구현하시오.
+//
+//		[입력]
+//		5640
+//
+//		[출력]
+//		500원: 11개
+//		100원: 1개
+//		50원: 0개
+//		10원: 4개
+//		1원: 0개
+		
+		
+	}
+
+}
