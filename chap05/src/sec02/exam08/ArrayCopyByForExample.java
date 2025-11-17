ae5a445053eee048d2a1fb7189ea807c65601306
geoblo <geoblo@naver.com>
2025-11-17 11:11:12 +0900

    up

---
+++ chap05/src/sec02/exam08/ArrayCopyByForExample.java
@@ -0,0 +1,14 @@
+package sec02.exam08;
+
+public class ArrayCopyByForExample {
+
+	public static void main(String[] args) {
+		// 배열은 한 번 생성되면 크기를 변경할 수 없기 때문에, 
+		// 추가 입력이 있을 때마다 새로운 배열을 만들고 기존 데이터를 복사하는 방식으로 처리
+		// 실무에서는 잘 사용하지 않고, ArrayList처럼 크기 조절이 자유로운 컬렉션을 보통 사용
+		
+		
+		
+	}
+
+}
