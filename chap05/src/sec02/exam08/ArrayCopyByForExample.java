f588284cf5f6ef92d537657dffcb00ddce230f46
geoblo <geoblo@naver.com>
2025-11-17 11:27:40 +0900

    up

--- chap05/src/sec02/exam08/ArrayCopyByForExample.java
+++ chap05/src/sec02/exam08/ArrayCopyByForExample.java
@@ -7,8 +7,17 @@ public class ArrayCopyByForExample {
 		// 추가 입력이 있을 때마다 새로운 배열을 만들고 기존 데이터를 복사하는 방식으로 처리
 		// 실무에서는 잘 사용하지 않고, ArrayList처럼 크기 조절이 자유로운 컬렉션을 보통 사용
 		
+		// 1. for문 이용
+		int[] oldIntArray = {1, 2, 3};
+		int[] newIntArray = new int[5];
 		
+		for (int i = 0; i < oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
 		
+		for (int i = 0; i < newIntArray.length; i++) {
+			System.out.print(newIntArray[i] + ", ");
+		}
 	}
 
 }
