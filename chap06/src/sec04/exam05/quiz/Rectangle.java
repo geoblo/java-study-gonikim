a4544e7789aba31d83238d8e1a07985bcfd47c0a
geoblo <geoblo@naver.com>
2025-11-18 16:38:58 +0900

    quiz

---
+++ chap06/src/sec04/exam05/quiz/Rectangle.java
@@ -0,0 +1,16 @@
+package sec04.exam05.quiz;
+
+public class Rectangle {
+	int width;
+    int height;
+
+    public void area() {
+        int result = width * height;
+        System.out.println("면적: " + result);
+    }
+
+    public void length() {
+        int result = (width + height) * 2;
+        System.out.println("둘레: " + result);
+    }
+}
