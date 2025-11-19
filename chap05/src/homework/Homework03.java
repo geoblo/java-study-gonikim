a0901f50ed1cc5bcc358ab52beedfcf2466ff00c
geoblo <geoblo@naver.com>
2025-11-19 09:52:23 +0900

    up

---
+++ chap05/src/homework/Homework03.java
@@ -0,0 +1,35 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Homework03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int length;
+		
+		// 유효한 값을 입력 받을 때까지 반복
+		while (true) {
+			System.out.print("3이상의 홀수를 입력하세요: ");
+			length = sc.nextInt();
+
+			if (length >= 3 && length % 2 == 1) {
+				break;
+			} else {
+				System.out.println("다시 입력하세요.");
+			}
+		}
+		
+		int[] arr = new int[length];
+		
+		for (int i = 0; i < length; i++) {
+			arr[i] = (i <= length / 2) ? i + 1 : length - i;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+            if (i > 0) System.out.print(", ");
+            System.out.print(arr[i]);
+        }
+	}
+
+}
