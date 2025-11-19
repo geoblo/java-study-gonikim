7cec611b654ed9fb110486406b2b4f5c63096aea
geoblo <geoblo@naver.com>
2025-11-19 10:13:38 +0900

    up

--- chap05/src/homework/Homework06.java
+++ chap05/src/homework/Homework06.java
@@ -1,9 +1,52 @@
 package homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Homework06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("배열의 크기를 입력하세요: ");
+        int size = sc.nextInt();
+        sc.nextLine(); // 개행 문자 제거
+
+        String[] arr = new String[size];
+        
+        for (int i = 0; i < arr.length; i++) { // arr.length == size
+            System.out.print((i + 1) + "번째 문자열: ");
+            arr[i] = sc.nextLine();
+        }
+
+        while (true) {
+            System.out.print("더 값을 입력하시겠습니까?(Y/N): ");
+            String answer = sc.nextLine();
+
+            if ("N".equalsIgnoreCase(answer)) {
+                break;
+            } else if ("Y".equalsIgnoreCase(answer)) {
+                System.out.print("더 입력하고 싶은 개수: ");
+                int addCount = sc.nextInt();
+                sc.nextLine(); // 개행 문자 제거
+                
+                // Arrays.copyOf를 이용해 크기 확장
+                arr = Arrays.copyOf(arr, arr.length + addCount);
+
+                // 추가된 부분에 새 문자열 입력
+                for (int i = arr.length - addCount; i < arr.length; i++) {
+                    System.out.print((i + 1) + "번째 문자열: ");
+                    arr[i] = sc.nextLine();
+                }
+            } else {
+                System.out.println("입력할 수 없는 값입니다.");
+            }
+        }
+
+        // 전체 출력
+        for (String str : arr) {
+            System.out.println(str);
+        }
 
 	}
 
--- chap05/src/homework/Homework08.java
+++ chap05/src/homework/Homework08.java
@@ -1,9 +1,50 @@
 package homework;
 
+import java.util.Arrays;
+
 public class Homework08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] menu = {"Burger", "Pizza", "Pasta"};
+        String[] add = {"Salad", "Soup"};
+
+        // menu 배열을 기반으로 길이를 늘려 새로운 배열 생성
+        // 방법1: Arrays.copyOf 사용 
+        String[] todayMenu = Arrays.copyOf(menu, menu.length + add.length);
+
+        // 추가 메뉴를 복사해서 붙이기
+        for (int i = 0; i < add.length; i++) {
+            todayMenu[menu.length + i] = add[i];
+        }
+
+        // 방법2: System.arraycopy(가장 빠름) 사용
+        String[] todayMenu2 = new String[menu.length + add.length];
+
+        System.arraycopy(menu, 0, todayMenu2, 0, menu.length);
+        System.arraycopy(add, 0, todayMenu2, menu.length, add.length);
+
+        // 출력
+        System.out.print("어제 메뉴판: ");
+        for (int i = 0; i < menu.length; i++) {
+        	if (i > 0) System.out.print(", ");
+            System.out.print(menu[i]);
+        }
+
+        System.out.println();
+
+        System.out.print("오늘 메뉴판: ");
+        for (int i = 0; i < todayMenu.length; i++) {
+        	if (i > 0) System.out.print(", ");
+            System.out.print(todayMenu[i]);
+        }
+        
+        System.out.println();
+        
+        System.out.print("오늘 메뉴판2: ");
+        for (int i = 0; i < todayMenu2.length; i++) {
+        	if (i > 0) System.out.print(", ");
+            System.out.print(todayMenu2[i]);
+        }
 
 	}
 
