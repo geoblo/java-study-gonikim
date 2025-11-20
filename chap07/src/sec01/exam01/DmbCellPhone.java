9a232b4c8484a5c48bde59ada3a1137d3abe5328
geoblo <geoblo@naver.com>
2025-11-20 10:31:25 +0900

    up

--- chap07/src/sec01/exam01/DmbCellPhone.java
+++ chap07/src/sec01/exam01/DmbCellPhone.java
@@ -1,5 +1,23 @@
 package sec01.exam01;
 
-public class DmbCellPhone {
-
+// 자식 클래스: 일반적인 특성은 물려받고 추가로 자식만이 갖는 멤버(필드, 메소드) 작성
+// 여기서는 CellPhone을 확장하여 DMB 기능만 추가된 클래스
+public class DmbCellPhone extends CellPhone {
+	// 필드
+	int channel;
+	
+	// 생성자
+	
+	
+	// 메소드
+	void turnOnDmb() {
+		System.out.println("채널 " + channel + "번 DMB 방송 수신을 시작합니다.");
+	}
+	void changeChannelDmb(int channel) {
+		this.channel = channel;
+		System.out.println("채널 " + channel + "번으로 바꿉니다.");
+	}
+	void turnOffDmb() {
+		System.out.println("DMB 방송 수신을 멈춥니다.");
+	}
 }
