28d8e45bc05138720f02ed6a809c6ee8b2cd9d00
geoblo <geoblo@naver.com>
2025-11-20 15:24:21 +0900

    u

---
+++ chap07/src/sec02/exam01/PromotionExample.java
@@ -0,0 +1,14 @@
+package sec02.exam01;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		B b = new B();
+		C c = new C();
+		D d = new D();
+		E e = new E();
+		
+		
+	}
+
+}
