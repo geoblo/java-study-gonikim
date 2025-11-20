8cc790bd241d3df3383e47602c3597d60cd40207
geoblo <geoblo@naver.com>
2025-11-20 14:29:31 +0900

    up

---
+++ chap07/src/sec01/exam07/package2/C.java
@@ -0,0 +1,12 @@
+package sec01.exam07.package2;
+
+import sec01.exam07.package1.A;
+
+public class C {
+	// 다른 패키지에서 접근 불가
+//	public void method() {
+//		A a = new A();
+//		a.field = "value";
+//		a.method();
+//	}
+}
