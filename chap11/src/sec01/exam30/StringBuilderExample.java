d6fbfeac3a34517662f1fba938b9f83d37d783e3
geoblo <geoblo@naver.com>
2025-11-27 11:50:04 +0900

    ㅕ

---
+++ chap11/src/sec01/exam30/StringBuilderExample.java
@@ -0,0 +1,17 @@
+package sec01.exam30;
+
+public class StringBuilderExample {
+
+	public static void main(String[] args) {
+		// StringBuilder
+		// 문자열을 변경 가능한 객체로 다루기 위해 만들어졌으며,
+		// 문자열을 이어붙이거나 수정할 때 보다 효율적이고 성능상 유리
+		// 다중 연산 시 메모리 절약 가능
+		
+		
+		
+		
+		
+	}
+
+}
