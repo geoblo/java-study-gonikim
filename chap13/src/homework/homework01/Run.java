f1467dd90a1298525a6eb0508c24e2ec29ddf292
geoblo <geoblo@naver.com>
2025-11-28 17:32:56 +0900

    풀이

---
+++ chap13/src/homework/homework01/Run.java
@@ -0,0 +1,12 @@
+package homework.homework01;
+
+import homework.homework01.view.BookMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		BookMenu bookMenu = new BookMenu();
+		bookMenu.mainMenu();
+	}
+
+}
