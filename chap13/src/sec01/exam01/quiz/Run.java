747c4ff3227553ba07dbe32ee75a3a7ba53c8810
geoblo <geoblo@naver.com>
2025-11-28 11:45:37 +0900

    up

--- chap13/src/sec01/exam01/quiz/Run.java
+++ chap13/src/sec01/exam01/quiz/Run.java
@@ -1,5 +1,10 @@
 package sec01.exam01.quiz;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Run {
 //	Quiz
 //	영화관 좌석의 목록 정보를 가지고 있는 ArrayList를 생성합니다.
@@ -37,15 +42,54 @@ public class Run {
 //	좌석 정렬 후: [A1, A2, A3, B1, B2]
 	
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		
+//		1. ArrayList에 초기 좌석 A2, A1, B1, B2를 추가합니다.
+		List<String> seats = new ArrayList<String>();
+		seats.add("A2");
+		seats.add("A1");
+		seats.add("B1");
+		seats.add("B2");
 		
+//		2. ArrayList의 초기 좌석 정보를 추가한 후 출력하세요. 
+		System.out.println("초기 좌석 목록: " + seats);
 		
+//		3. 영화관 좌석중에서 3번째 좌석에 A3 좌석을 추가합니다. 
+		seats.add(2, "A3");
 		
+//		4. 그 다음, 영화관 좌석 목록을 출력하세요.
+		System.out.println("A3 좌석 추가 후: " + seats);
 		
+//		5. 예약하고 싶은 좌석의 정보를 키보드로 입력받습니다. 
+		System.out.print("예약 좌석: ");
+		String reservedSeat = sc.nextLine();
 		
+//		6. 만일 사용자가 입력한 좌석이 존재하는 좌석이라면 
+//		해당 좌석의 정보뒤에 (예약)이라는 문자열을 추가해주세요.
+		int index = 0;
+		if ((index = seats.indexOf(reservedSeat)) != -1) {
+//			seats.set(index, reservedSeat + "(예약)");
+			seats.set(index, seats.get(index) + "(예약)");
+		} else {
+			System.out.println(reservedSeat + "은(는) 존재하지 않는 좌석입니다.");
+		}
+		System.out.println("좌석 예약 후: " + seats);
 		
+//		7. 삭제하고 싶은 좌석의 정보를 키보드로 입력받습니다. 
+		System.out.print("삭제 좌석: ");
+		String removedSeat = sc.nextLine();
 		
+//		8. 만일 사용자가 입력하는 좌석이 존재하는 좌석이라면 해당 좌석을 제거한 후 목록을 출력해주세요. 
+		if (seats.contains(removedSeat)) {
+			seats.remove(removedSeat);
+		} else {
+			System.out.println(removedSeat + " 좌석은 목록에 없습니다.");
+		}
+		System.out.println("좌석 삭제 후: " + seats);
+		
+//		9. 마지막으로 오름차순 정렬한 후에 출력합니다.
+		Collections.sort(seats);
+		System.out.println("좌석 정렬 후: " + seats);
 		
 	}
 
