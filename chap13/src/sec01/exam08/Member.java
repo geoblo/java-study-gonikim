c344cfe147ab87738c22a882007917bcece816ed
geoblo <geoblo@naver.com>
2025-12-02 15:17:22 +0900

    up

---
+++ chap13/src/sec01/exam08/Member.java
@@ -0,0 +1,53 @@
+package sec01.exam08;
+
+import java.util.Objects;
+
+public class Member implements Comparable<Member> {
+//public class Member {
+	private String name;
+	private int age;
+
+	public Member(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Member))
+			return false;
+		Member other = (Member) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int compareTo(Member other) {
+//		return this.age - other.age;
+		
+		// 비교 연산
+		if (this.age < other.age) return -1; 
+		else if (this.age > other.age) return 1;
+		else return 0;
+	}
+	
+}
--- chap13/src/sec01/exam08/TreeSetExample.java
+++ chap13/src/sec01/exam08/TreeSetExample.java
@@ -1,5 +1,6 @@
 package sec01.exam08;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -15,7 +16,10 @@ public class TreeSetExample {
 		
 //		Set<String> set = new HashSet<String>();
 //		Set<String> set = new LinkedHashSet<String>();
-		Set<String> set = new TreeSet<String>();
+//		Set<String> set = new TreeSet<String>(); // Comparable로 구현한 기본 정렬 기준을 사용
+		
+		// 기본 정렬 기준 외 - TreeSet 생성자에 Comparator를 매개값으로 전달
+		Set<String> set = new TreeSet<String>(Collections.reverseOrder());
 		
 		set.add("동");
 		set.add("서");
@@ -23,7 +27,13 @@ public class TreeSetExample {
 		set.add("북");
 		System.out.println(set);
 		
-		
+		// 사용자 정의 객체로 정렬 테스트
+		// 1. Comparable로 기본 정렬 기준 구현 또는 2. TreeSet 생성 시 Comparator 구현으로 정렬 기준 제공
+		Set<Member> memberSet = new TreeSet<Member>();
+		memberSet.add(new Member("김재현", 30)); // 정렬 기준이 없으면 예외 발생
+		memberSet.add(new Member("이수진", 25));
+		memberSet.add(new Member("박민수", 40));
+		System.out.println(memberSet);
 		
 	}
 
