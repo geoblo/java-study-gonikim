b3669262576f3872a85f300a50d312a3a8e53bc1
geoblo <geoblo@naver.com>
2025-12-05 15:30:43 +0900

    up

---
+++ chap14/src/sec03/exam03/quiz/Quiz1.java
@@ -0,0 +1,26 @@
+package sec03.exam03.quiz;
+
+public class Quiz1 {
+//	Quiz
+//	C 드라이브 아래에 test/sub 폴더를 생성하는데
+//	아래와 같이 test/sub 경로에 오늘 날짜 이름의 폴더를 생성하세요.
+//	오늘 날짜 정보는 LocalDate를 통해서 가져옵니다.
+
+//	C:/test/sub/2025/06/06
+
+//	해당 폴더가 없을 때만 생성될 수 있도록 조건문을 반드시 추가하세요.
+
+//	상황에 맞게 아래의 문장을 출력하세요.
+
+//	[출력]
+//	폴더 생성 성공
+//	폴더 생성 실패
+//	이미 존재하는 폴더
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
