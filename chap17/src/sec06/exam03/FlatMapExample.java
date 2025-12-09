9fed57067b7e3631a12c1e7348e1af39df52e256
geoblo <geoblo@naver.com>
2025-12-09 14:41:03 +0900

    up

---
+++ chap17/src/sec06/exam03/FlatMapExample.java
@@ -0,0 +1,36 @@
+package sec06.exam03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMapExample {
+
+	public static void main(String[] args) {
+		// 문장 스트림을 단어 스트림으로 변환
+		List<String> list1 = new ArrayList<String>();
+		list1.add("this is java");
+		list1.add("i am a best developer");
+		list1.stream() // 오리지널 스트림: Stream<String> => 문자열이 2개 흘러감
+			.flatMap(data -> Arrays.stream(data.split(" "))) // 3개 짜리 + 5개 짜리 = 문자열 8개가 흘러가는 새로운 스트림 생성
+			.forEach(word -> System.out.println(word));
+		
+		System.out.println();
+		
+		// 문자열 숫자 목록 스트림을 숫자 스트림으로 변환
+		List<String> list2 = Arrays.asList("10, 20, 30", "40, 50");
+		list2.stream() // 2개의 문자열이 흘러감
+			.flatMapToInt(data -> { // int 값으로 펼치겠다. 예: "10, 20, 30" -> 10 20 30
+				String[] strArr = data.split(",");
+				int[] intArr = new int[strArr.length];
+				for (int i = 0; i < strArr.length; i++) {
+					intArr[i] = Integer.parseInt(strArr[i].trim());
+				}
+				return Arrays.stream(intArr); // 3개의 IntStream 리턴 + 2개의 IntStream 리턴
+			})
+			.forEach(number -> System.out.println(number)); // 5개의 int가 흘러가는 IntStream 출력
+		
+		// <? super String, ? extends IntStream>
+	}
+
+}
