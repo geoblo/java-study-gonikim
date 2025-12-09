a55e7fdd39da489a0c749ca9f855c4c6d6f37abc
geoblo <geoblo@naver.com>
2025-12-09 15:28:40 +0900

    up

---
+++ chap17/src/sec07/exam01/Student.java
@@ -0,0 +1,35 @@
+package sec07.exam01;
+
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	@Override
+	public int compareTo(Student other) {
+		// 점수 기준 오름차순
+//		if (this.score < other.score) {
+//			return -1;
+//		} else if (this.score > other.score) {
+//			return 1;
+//		} else {
+//			return 0;			
+//		}
+		
+		// 위 비교 연산을 이미 구현한 메소드
+		return Integer.compare(this.score, other.score);
+	}
+	
+}
